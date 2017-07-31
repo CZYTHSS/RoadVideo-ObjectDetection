c10342dabb3db2a153f07cd7c024bd243c39fcb0
Mark Chen <chenzhengyu14thss@163.com>
2017-07-31 01:20:26 -0700

    add RoadEdge detection, already able to detect the edge of the road. Version 1.0

--- main.cpp
+++ main.cpp
@@ -14,14 +14,20 @@ using namespace std;
 using namespace cv;
 
 void extract_video();	//函数内可以修改读取文件名，输出文件名，编码格式以及帧数范围
-void FindLine(Mat source, int arg);
+vector<Vec2f> FindLine(Mat source, int arg);
 void ObjectDetect(VideoCapture cap);
-void FindRoadEdge(VideoCapture cap);
-void LineExtract(vector<Vec4i> lines, Mat img);		//从Hough Transform中提取的线段数据中找出代表路基的两条
+vector<Vec2f> FindRoadEdge(VideoCapture cap);
+vector<Vec2f> LineExtract(vector<Vec4i> lines, Mat img);		//从Hough Transform中提取的线段数据中找出代表路基的两条
+void DrawLine(Vec2f edge, Mat img);
 bool Vec4isort(Vec4i i, Vec4i j) {
 	int a = (i[1] < i[3] ? i[1] : i[3]);
 	int b = (j[1] < j[3] ? j[1] : j[3]);
 	if (a < b) return 1;
+	else return 0;
+}
+
+bool Vec2fsort(Vec2f i, Vec2f j) {
+	if (i[1] < j[1]) return 1;
 	else return 0;
 }
 
@@ -30,16 +36,22 @@ int main()
 {
 	//extract_video();
 
-	VideoCapture cap("data/clip_1.mp4");
+	VideoCapture cap("data/clip_9.mp4");
 
 	if (!cap.isOpened()) {
 		cerr << "can't open the video file!" << endl;
 		return -1;
 	}
 	
-	FindRoadEdge(cap);
-	//ObjectDetect(cap);
-	//FindLine(cap, 1);
+	vector<Vec2f> edges = FindRoadEdge(cap);		//这里返回检测部分的路边界，以极坐标表示，edges[0],edges[1]分别是上下边界。edges[0][0]是r, edges[0][1]是theta。 edges[1]同理
+	Mat img = imread("background.png");
+	DrawLine(edges[0], img);
+	DrawLine(edges[1], img);
+
+	namedWindow("edges", WINDOW_NORMAL);
+	imshow("edges", img);
+	waitKey(0);
+
 	return 0;
 }
 
@@ -82,9 +94,9 @@ void extract_video() {
 /* This is a standalone program. Pass an image name as the first parameter
 of the program.  Switch between standard and probabilistic Hough transform
 by changing arg from 1 to 0 and back */
-void FindLine(Mat source, int arg)
+vector<Vec2f> FindLine(Mat source, int arg)
 {
-	namedWindow("src", WINDOW_NORMAL);
+	//namedWindow("src", WINDOW_NORMAL);
 	Mat src, dst, color_dst, frame1, frame2, diff;
 	
 	src = source.clone();
@@ -93,6 +105,8 @@ void FindLine(Mat source, int arg)
 	cvtColor(dst, color_dst, CV_GRAY2BGR);
 	//imshow("color_dst", color_dst);
 	//waitKey(10000);
+	Mat edge(color_dst.rows, color_dst.cols, CV_8UC3, Scalar(0, 0, 0));
+	vector<Vec2f> edges;
 
 	if (arg == 0) {
 		vector<Vec2f> lines;		//Vec2f定义:typedef Vec<float, 2> Vec2f;
@@ -113,25 +127,25 @@ void FindLine(Mat source, int arg)
 	}
 	else {
 		vector<Vec4i> lines;		//Vec4i定义：typedef Vec<int, 4> Vec4i;
-		HoughLinesP(dst, lines, 1, CV_PI / 180, 80, 50, 100);
+		HoughLinesP(dst, lines, 1, CV_PI / 180, 80, 30, 30);
 		for (size_t i = 0; i < lines.size(); i++)
 		{
-			line(color_dst, Point(lines[i][0], lines[i][1]),
-				Point(lines[i][2], lines[i][3]), Scalar(0, 0, 255), 2, 8);
+			//line(color_dst, Point(lines[i][0], lines[i][1]),Point(lines[i][2], lines[i][3]), Scalar(0, 0, 255), 2, 8);
 			//cout << "(" << lines[i][0] << "," << lines[i][1] << ")," << "(" << lines[i][2] << "," << lines[i][3] << ")" << endl;	//查看每一条线坐标
 		}
 		//cout << lines.size();
-		LineExtract(lines, color_dst);
+		edges = LineExtract(lines, edge);
 	}
 
-	namedWindow("Source", WINDOW_NORMAL);
-	imshow("Source", src);
+	//namedWindow("Source", WINDOW_NORMAL);
+	//imshow("Source", src);
 
-	namedWindow("Detected Lines", WINDOW_NORMAL);
-	imshow("Detected Lines", color_dst);
-	//imwrite("data/RoadEdge9.png", color_dst);
+	
+
+	//imwrite("data/RoadEdge8.png", edge);
 
 	waitKey(0);
+	return edges;
 }
 
 void ObjectDetect(VideoCapture cap)
@@ -161,7 +175,7 @@ void ObjectDetect(VideoCapture cap)
 		threshold(gray_diff, gray_diff, 15, 255, CV_THRESH_BINARY);
 
 		gray1 = gray1 / 2;
-		imshow("video", gray1);
+		//imshow("video", gray1);
 		int key_value = waitKey(15);
 		if (key_value != 255)
 		{
@@ -174,42 +188,42 @@ void ObjectDetect(VideoCapture cap)
 	}
 }
 
-void FindRoadEdge(VideoCapture cap)
+vector<Vec2f> FindRoadEdge(VideoCapture cap)
 {
 	
 	Mat background, src, current;
 	
-	//for (int i = 1; i < 600; i++) {
-	//	cap >> src;
-	//	if (i == 1) {
-	//		background = src.clone();
-	//		current = src.clone();
-	//	}
-	//	else {
-	//		background = (current * (i - 1) + src) / i;
-	//		current = background.clone();
-	//	}
-	//	cout << i << endl;
-	//	//imshow("video", background);
-	//	//waitKey(1000);
-	//	/*if (waitKey(20) != 255) {
-	//		int n = waitKey(10000);
-	//		if (n == 27) break;
-	//	}*/
-	//}
-	//imwrite("background.png", background);
+	//找出一个平均背景
+	for (int i = 1; i < 600; i++) {
+		cap >> src;
+		if (i == 1) {
+			background = src.clone();
+			current = src.clone();
+		}
+		else {
+			background = (current * (i - 1) + src) / i;
+			current = background.clone();
+		}
+		cout << i << endl;
+		//imshow("video", background);
+		//waitKey(1000);
+		/*if (waitKey(20) != 255) {
+			int n = waitKey(10000);
+			if (n == 27) break;
+		}*/
+	}
+	imwrite("background.png", background);
 	
 	background = imread("background.png");
-	namedWindow("video", WINDOW_NORMAL);
-	/*imshow("video", background);
-	waitKey(0);*/
+	//namedWindow("video", WINDOW_NORMAL);
+
 
 	Mat gray_bg;
 	cvtColor(background, gray_bg, CV_RGB2GRAY);
-	imshow("video", gray_bg);
+	//imshow("video", gray_bg);
 	waitKey(0);
 	threshold(gray_bg, gray_bg, 0, 255, CV_THRESH_OTSU);
-	imshow("video", gray_bg);
+	//imshow("video", gray_bg);
 	waitKey(0);
 
 	Mat result;
@@ -241,44 +255,14 @@ void FindRoadEdge(VideoCapture cap)
 	result.col(0).setTo(Scalar(255));
 	result.col(result.cols - 1).setTo(Scalar(255));
 
-	imshow("video", result);
+	//imshow("video", result);
 	waitKey(0);
-	//imwrite("data/RoadEdge1.png", result);
-	FindLine(result,1);
-	//for (;;) {
-	//	frame1 = frame2.clone();
-	//	cap >> frame2;
-	//	if (frame2.empty())
-	//		break;
-	//	//diff = frame1 - frame2;
-	//	Mat gray_diff, gray1, gray2;
-	//	cvtColor(frame1, gray1, COLOR_RGB2GRAY);
-	//	cvtColor(frame2, gray2, COLOR_RGB2GRAY);
-	//	//cvtColor(diff, gray_diff, COLOR_RGB2GRAY);
 
-	//	threshold(gray1, gray1, 15, 255, CV_THRESH_OTSU);
-	//	//threshold(gray2, gray2, 100, 255, CV_THRESH_BINARY);
-
-	//	gray_diff = gray1 - gray2;
-	//	//threshold(gray_diff, gray_diff, 15, 255, CV_THRESH_OTSU);
-	//	GaussianBlur(gray_diff, gray_diff, Size(3, 5), 0, 0);
-	//	threshold(gray_diff, gray_diff, 15, 255, CV_THRESH_BINARY);
-
-	//	gray1 = gray1 / 2;
-	//	imshow("video", gray1);
-	//	int key_value = waitKey(15);
-	//	if (key_value != 255)
-	//	{
-	//		if (key_value == 27) break;		//press ESC to break
-	//		else {
-	//			key_value = waitKey(100000);
-	//			if (key_value == 27) break;
-	//		}
-	//	}
-	//}
+	vector<Vec2f> edges = FindLine(result,1);
+	return edges;
 }
 
-void LineExtract(vector<Vec4i> lines, Mat img)
+vector<Vec2f> LineExtract(vector<Vec4i> lines, Mat img)
 {
 	cout << lines.size() << endl;
 	sort(lines.begin(), lines.end(), Vec4isort);
@@ -289,9 +273,11 @@ void LineExtract(vector<Vec4i> lines, Mat img)
 	cout << "(" << down[0] << "," << down[1] << ")," << "(" << down[2] << "," << down[3] << ")" << endl;
 	Vec2d v_up((up[2] - up[0]), (up[3] - up[1]));
 	Vec2d v_down((down[2] - down[0]), (down[3] - down[1]));	//将up down两条线变成两个二位向量，便于计算夹角。
-	vector<int> up_flags;
+	vector<int> up_flags, down_flags;
+	
+	//计算上边界
 	for (size_t i = 1; i < lines.size() - 1; i++) {
-		double param = 10;
+		double param = 3.5;
 		double theta1, theta2;	//theta1表示当前线段与up/down方向的夹角的cos，theta2表示当前线段起点与up/down起点的连线与up/down线段的夹角的cos
 		Vec2d temp((lines[i][2] - lines[i][0]), (lines[i][3] - lines[i][1]));
 		theta1 = (temp[0] * v_up[0] + temp[1] * v_up[1])/(sqrt(v_up[0] * v_up[0] + v_up[1] * v_up[1]) * sqrt(temp[0] * temp[0] + temp[1] * temp[1]));
@@ -303,18 +289,94 @@ void LineExtract(vector<Vec4i> lines, Mat img)
 				up_flags.push_back(i);
 			}
 		}
+	}
 
-		//cout << temp[0] << " " << temp[1] << endl;
+	//计算下边界
+	for (size_t i = 1; i < lines.size() - 1; i++) {
+		double param = 3.5;
+		double theta1, theta2;	//theta1表示当前线段与down/down方向的夹角的cos，theta2表示当前线段起点与up/down起点的连线与up/down线段的夹角的cos
+		Vec2d temp((lines[i][2] - lines[i][0]), (lines[i][3] - lines[i][1]));
+		theta1 = (temp[0] * v_down[0] + temp[1] * v_down[1]) / (sqrt(v_down[0] * v_down[0] + v_down[1] * v_down[1]) * sqrt(temp[0] * temp[0] + temp[1] * temp[1]));
+		if (theta1 > cos(param * PI / 180.0)) {
+			temp[0] = lines[i][2] - down[0];
+			temp[1] = lines[i][3] - down[1];
+			theta2 = (temp[0] * v_down[0] + temp[1] * v_down[1]) / (sqrt(v_down[0] * v_down[0] + v_down[1] * v_down[1]) * sqrt(temp[0] * temp[0] + temp[1] * temp[1]));
+			if (theta2 > cos(param * PI / 180.0)) {
+				down_flags.push_back(i);
+			}
+		}
 	}
 
 	for (size_t i = 0; i < up_flags.size(); i++) {
-		cout << up_flags[i] << endl;
+		line(img, Point(lines[up_flags[i]][0], lines[up_flags[i]][1]),
+			Point(lines[up_flags[i]][2], lines[up_flags[i]][3]), Scalar(0, 0, 255), 2, 8);
+	}
+	for (size_t i = 0; i < down_flags.size(); i++) {
+		line(img, Point(lines[down_flags[i]][0], lines[down_flags[i]][1]),
+			Point(lines[down_flags[i]][2], lines[down_flags[i]][3]), Scalar(0, 0, 255), 2, 8);
 	}
 
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		line(img, Point(lines[i][0], lines[i][1]),
-			Point(lines[i][2], lines[i][3]), Scalar(0, 0, 255), 2, 8);
-		cout << "(" << lines[i][0] << "," << lines[i][1] << ")," << "(" << lines[i][2] << "," << lines[i][3] << ")" << endl;	//查看每一条线坐标
+	//namedWindow("Detected Lines", WINDOW_NORMAL);
+	//imshow("Detected Lines", img);
+	waitKey(0);
+	Mat dst;
+
+	cvtColor(img, dst, CV_RGB2GRAY);
+	vector<Vec2f> v2f_lines;		//Vec2f定义:typedef Vec<float, 2> Vec2f;
+	HoughLines(dst, v2f_lines, 1, CV_PI / 180, 160);
+
+
+	/*imshow("Detected Lines", img);
+	waitKey(0);*/
+	cout << endl << "Hough Transform:" << endl;
+	for (size_t i = 0; i < v2f_lines.size(); i++) {
+		cout << v2f_lines[i][0] << "," << v2f_lines[i][1] << endl;
 	}
+
+	vector<Vec2f> up_lines, down_lines;
+	for (size_t i = 0; i < v2f_lines.size(); i++) {
+		if (v2f_lines[i][0] < 500) up_lines.push_back(v2f_lines[i]);
+		else down_lines.push_back(v2f_lines[i]);
+	}
+	sort(up_lines.begin(), up_lines.end(), Vec2fsort);
+	sort(down_lines.begin(), down_lines.end(), Vec2fsort);
+	Vec2f up_edge, down_edge;
+	if (up_lines.size() == 0) cerr << "ERROR: up road edge failed to detect!" << endl;
+	else if (up_lines.size() % 2 == 1) {
+		up_edge = up_lines[up_lines.size() / 2];
+	}
+	else {
+		up_edge = up_lines[(up_lines.size() - 1) / 2];
+	}
+
+	if (down_lines.size() == 0) cerr << "ERROR: down road edge failed to detect!" << endl;
+	else if (down_lines.size() % 2 == 1) {
+		down_edge = down_lines[down_lines.size() / 2];
+	}
+	else {
+		down_edge = down_lines[(down_lines.size() - 1) / 2];
+	}
+
+	DrawLine(up_edge, img);
+	DrawLine(down_edge, img);
+	//imshow("Detected Lines", img);
+	waitKey(0);
+
+	vector<Vec2f> edges;
+	edges.push_back(up_edge);
+	edges.push_back(down_edge);
+	return edges;
+
+}
+
+void DrawLine(Vec2f edge, Mat img) {
+	float rho = edge[0];
+	float theta = edge[1];
+	double a = cos(theta), b = sin(theta);
+	double x0 = a*rho, y0 = b*rho;
+	Point pt1(cvRound(x0 + 1000 * (-b)),
+		cvRound(y0 + 1000 * (a)));
+	Point pt2(cvRound(x0 - 1000 * (-b)),
+		cvRound(y0 - 1000 * (a)));
+	line(img, pt1, pt2, Scalar(0, 255, 255), 3, 8);
 }
